9c34e6d2d7b9fc69ede6d85482a861920d69db14
chenmeng <1596941391@qq.com>
2025-12-16 01:01:10 +0800

    12

--- api/auth/session.ts
+++ api/auth/session.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { verifyToken } from '../../lib/auth.js';
-import { getUserById } from '../../lib/db.js';
+import { verifyToken } from '../lib/auth.js';
+import { getUserById } from '../lib/db.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'GET') {
